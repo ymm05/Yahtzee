5b1394123c8d2afc2777a002d346e5ec0cc39c79
Roger Jaffe <rogerjaffe@gmail.com>
2022-09-12 11:27:48 -0700

    Initial commit

---
+++ RenameThisClass.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RenameThisClass
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
