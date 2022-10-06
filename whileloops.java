10ce7d2c884be47ba7cc35d784958ef3f6748bed
Yasin McCaskill <yasin.mccaskill@mrjaffesclass.com>
2022-10-06 14:24:43 -0700

    10/6

---
+++ whileloops.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class whileloops
+{ 
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+
+    public void run () {
+        for (int i = 0; i < 10; i = i+ 1) {
+            System.out.println("i is equal to "+i);
+        }
+        
+        for (int i = 0, j = 0; i < 10 && j < 5; i++, j=j+2) {
+           System.out.println("Compund for loop i="+i+" j="+j); 
+        }
+    }
+
+    public static void main (String[] args)
+    {
+        whileloops hw = new whileloops();
+        hw.run();
+    }
+}
